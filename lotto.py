6759542036dde93f544065857b886646f3e87a14
Adam Oakes <a.oakes247@gmail.com>
2020-01-20 21:20:14 +0000

    Changed some code re: img

--- lotto.py
+++ lotto.py
@@ -1,17 +1,16 @@
 # Widgets:
 from Tkinter import *
-
 window = Tk()
-img = PhotoImage( file = '/home/adam/Documents/PythonRepo/LottoNumberPicker/logo.jpg')
-imgLbl = Label( window, image = img )
+img = PhotoImage( file = 'logo.jpg' )
+imgLbl = Label ( window, image = img )
 label1 = Label ( window, relief = 'groove', width = 2 )
 label2 = Label ( window, relief = 'groove', width = 2 )
 label3 = Label ( window, relief = 'groove', width = 2 )
 label4 = Label ( window, relief = 'groove', width = 2 )
 label5 = Label ( window, relief = 'groove', width = 2 )
 label6 = Label ( window, relief = 'groove', width = 2 )
-getBtn = Button( window )
-resBtn = Button( window )
+getBtn = Button ( window )
+resBtn = Button ( window )
 
 # Geometry:
 imgLbl.grid( row = 1, column = 1, rowspan = 2 )
